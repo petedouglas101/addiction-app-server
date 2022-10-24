104b5cab62f6d9faeab5c2abf5673cc58e9788da
Peter Douglas <peterpatrickdouglas@gmail.com>
2022-10-24 14:59:11 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
+
+app.use(bodyParser.json());
 app.use(authRoutes);
 
 const mongoURI =
@@ -20,8 +23,8 @@ mongoose.connection.on("error", (err) => {
   console.error("Error connecting to mongo", err);
 });
 
-app.get('/', (req, res) => {
-  res.send("Hello World");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,44 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const User = mongoose.model("User");
 
 const router = express.Router();
 
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
 
+  try {
     const user = new User({ email, password });
     await user.save();
-    res.send("You made a post request");
+
+    const token = jwt.sign({ userId: user._id }, "SECRET_KEY");
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email and password" });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(422).send({ error: "Incorrect password or email" });
+  }
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user.id }, "SECRET_KEY");
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: "Incorrect password or email" });
+  }
 });
 
 module.exports = router;
